85fdf80489d8969e5d4944a4251349c2cba01d68
Linda <szekereslin@gmail.com>
2025-10-15 22:29:06 +0200

    Update codeforces_replace-with-occurences.cpp

--- arrays/codeforces_replace-with-occurences.cpp
+++ arrays/codeforces_replace-with-occurences.cpp
@@ -1,42 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); // Fast I/O
+    ios::sync_with_stdio(false); cin.tie(0); // Fast input/output
 
     int t;
-    cin >> t;  // Number of test cases
+    cin >> t; // Number of test cases
     while (t--) {
         int n;
-        cin >> n;  // Size of the sequence
-        vector<int> val(n + 1), cnt(n + 1), res(n);  
-        // val[b] stores the assigned value for number b
-        // cnt[b] stores how many more times b can appear
-        // res stores the resulting sequence
+        cin >> n; // Size of the array b
+        vector<int> val(n + 1), cnt(n + 1), res(n); 
+        // val[b] will store the value assigned to elements with frequency b
+        // cnt[b] keeps track of how many more times we need to assign this value
+        // res stores the resulting array a
 
-        int x = 1;  // Counter for assigning unique values
+        int x = 1; // The next available number to assign in array a
+
         for (int i = 0; i < n; i++) {
             int b;
-            cin >> b;  // Read element
-            if (cnt[b] == 0) {  // First time seeing b
-                val[b] = x;     // Assign a new unique number to b
-                x++;            // Increment unique number counter
-                cnt[b] = b;     // Set count of remaining occurrences allowed
+            cin >> b; // Read the desired frequency for position i
+
+            if (cnt[b] == 0) { 
+                // If we haven't assigned a number for this frequency yet
+                val[b] = x; // Assign the next available number to this frequency
+                x++;        // Increment for the next new value
+                cnt[b] = b; // Set how many times this value should appear
             }
-            res[i] = val[b];    // Store assigned value in result
-            cnt[b]--;           // Decrease remaining occurrences
+
+            res[i] = val[b]; // Assign the corresponding value to array a
+            cnt[b]--;        // Decrease the remaining count for this value
         }
 
-        bool valid = true;  // Flag to check if sequence is valid
+        bool valid = true;
+        // After assignment, check if all counts are satisfied
         for (int i : cnt) {
-            if (i > 0) {  // If any number has leftover occurrences
-                cout << -1 << endl;  // Invalid sequence
-                valid = false;
-                break;
+            if (i > 0) { 
+                cout << -1 << endl; // If some count not satisfied, it's invalid
+                valid = false; 
+                break; 
             }
         }
 
-        if (valid) {  // If valid, print the resulting sequence
+        if (valid) {
+            // Print the constructed array a
             for (int i : res) cout << i << " ";
             cout << endl;
         }
