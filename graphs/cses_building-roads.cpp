1505d6bca941446e0c5bec5438b051a336401490
Linda <szekereslin@gmail.com>
2025-10-23 18:32:27 +0200

    Update cses_building-roads.cpp

--- graphs/cses_building-roads.cpp
+++ graphs/cses_building-roads.cpp
@@ -1,65 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> adj; // adjacency list of cities (roads)
+vector<vector<int>> adj; // adjacency list for cities
 vector<bool> vis; // visited array
-vector<int> ord, compid; // DFS order and component id for each node
 
-// DFS to explore connected component
-void dfs(int v) {
+// DFS to explore a connected component
+void dfs(int v, int id) {
     vis[v] = true;
     for (int u : adj[v])
-        if (!vis[u]) dfs(u);
-    ord.push_back(v);
-}
-
-// DFS to mark component id
-void dfs2(int v, int id) {
-    vis[v] = true;
-    compid[v] = id;
-    for (int u : adj[v])
-        if (!vis[u]) dfs2(u, id);
+        if (!vis[u]) dfs(u, id);
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
     adj.assign(n+1, vector<int>());
+    // read undirected roads
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
-        adj[b].push_back(a); // undirected graph
+        adj[b].push_back(a);
     }
 
     vis.assign(n+1, false);
-    // find DFS finishing order
+    vector<int> ids; // store representative nodes of components
+    // find all connected components
     for (int i = 1; i <= n; i++)
-        if (!vis[i]) dfs(i);
-    reverse(ord.begin(), ord.end());
-
-    vis.assign(n+1, false);
-    compid.assign(n+1, -1);
-    // assign component ids
-    for (int v : ord)
-        if (!vis[v]) {
-            dfs2(v, v);
+        if (!vis[i]) {
+            dfs(i, i);
+            ids.push_back(i);
         }
 
-    vector<bool> compids(n+1);
-    vector<int> ids;
-    // collect unique component representatives
-    for (int i = 1; i <= n; i++)
-        if (!compids[compid[i]]) {
-            compids[compid[i]] = true;
-            ids.push_back(compid[i]);
-        }
-
-    // if graph already connected
+    // print minimum number of roads to connect components
     if (ids.size() == 0) cout << "0\n";
-    else {
-        cout << ids.size()-1 << endl; // minimum roads = components - 1
-        for (int i = 1; i < (int)ids.size(); i++)
-            cout << ids[0] << " " << ids[i] << endl; // connect components
-    }
+    else cout << ids.size()-1 << endl;
+
+    // connect each component to the first one
+    for (int i = 1; i < (int)ids.size(); i++)
+        cout << ids[0] << " " << ids[i] << endl;
 }
