e206f5ab025cf51d3fd737b1ce2e3d9b7f98211f
Linda <szekereslin@gmail.com>
2025-10-24 21:48:42 +0200

    Update and rename prefix-sums/codeforces_cut-the-array.cpp to math/prefix-sum/codeforces_cut-the-array.cpp

--- math/prefix-sum/codeforces_cut-the-array.cpp
+++ math/prefix-sum/codeforces_cut-the-array.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
- 
+
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0);
- 
     int t;
     cin >> t;
     while (t--) {
