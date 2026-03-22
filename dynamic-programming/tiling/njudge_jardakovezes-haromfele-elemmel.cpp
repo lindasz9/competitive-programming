f0014527217cf7c67ce9813661e583c651c10ede
Linda <szekereslin@gmail.com>
2026-03-22 10:54:05 +0100

    Update njudge_jardakovezes-haromfele-elemmel.cpp

--- dynamic-programming/tiling/njudge_jardakovezes-haromfele-elemmel.cpp
+++ dynamic-programming/tiling/njudge_jardakovezes-haromfele-elemmel.cpp
@@ -7,11 +7,11 @@ int main() {
 
     vector<int> a(n+1), b(n+1);
     a[0]=1; a[1]=2; // a[i]: összes lehetséges kikövezés i hosszra
-    b[0]=0; b[1]=1; // b[i]: segéd DP állapot
+    b[0]=0; b[1]=1; // b[i]: összes lehetséges kikövezés i hosszra + kilógó rész
 
     for(int i=2;i<=n;i++){
-        b[i] = (a[i-1] + b[i-1] + a[i-2]) % 2023;  // segédállapot frissítése
-        a[i] = (b[i] + a[i-1] + a[i-2]*2 + b[i-1]*3) % 2023; // összes lehetőség
+        b[i] = (a[i-1] + b[i-1] + a[i-2]) % 2023;  // kilógó rész frissítése
+        a[i] = (b[i] + a[i-1] + a[i-2]*2 + b[i-1]*3) % 2023; // i hossz frissítése
     }
 
     cout<<a[n]<<'\n'; // kikövezések száma mod 2023
