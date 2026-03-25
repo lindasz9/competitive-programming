65c005c077fb9c5ce6b668ac2820162c5a832225
Linda <szekereslin@gmail.com>
2026-03-26 00:09:08 +0100

    Update njudge_utazas-keletre.cpp

--- graphs/DAG/njudge_utazas-keletre.cpp
+++ graphs/DAG/njudge_utazas-keletre.cpp
@@ -3,57 +3,57 @@ using namespace std;
 
 const int maxn = 100001;
 
-// adjacency list of directed flights
+// irányított repülőjáratok listája
 vector<int> adj[maxn];
 
-// visited array for topological sort
+// topologikus rendezéshez látogatottság jelzőtömb
 bool vis[maxn];
 
-// whether the position is losing for the current player
+// jelzi, hogy az adott városból indulva a kezdő játékos veszít-e
 bool islose[maxn];
 
-// DFS for topological sorting
+// DFS a topologikus sorrendhez
 void dfs(int v, vector<int>& ord){
     vis[v] = true;
     for(int u : adj[v])
         if(!vis[u])
             dfs(u, ord);
-    ord.push_back(v); // add after visiting all neighbors
+    ord.push_back(v); // a szomszédok feldolgozása után adjuk a várost
 }
 
 int main(){
     int n, m, q;
     cin >> n >> m >> q;
 
-    vector<int> s(q);
+    vector<int> s(q); // lekérdezések kezdővárosai
     for(int i = 0; i < q; i++) cin >> s[i];
 
-    // build the directed graph of flights
+    // gráf felépítése: irányított repülőjáratok
     for(int i = 0; i < m; i++){
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
     }
 
-    // topological order of cities
+    // topologikus sorrend előállítása
     vector<int> ord;
     for(int i = 1; i <= n; i++)
         if(!vis[i])
             dfs(i, ord);
 
-    // process cities in topological order (reverse for DP)
+    // városok feldolgozása topologikus sorrendben (DP)
     for(int x : ord){
         bool lose = true;
-        // if there is any neighbor which is losing, current city is winning
+        // ha bármelyik szomszéd veszít, a jelenlegi város nyerő
         for(int i : adj[x]){
             if(islose[i]) lose = false;
         }
-        islose[x] = lose;
+        islose[x] = lose; // aktuális város vesztes vagy nyerő
     }
 
-    // answer queries
+    // lekérdezések kiértékelése
     for(int x : s){
-        if(islose[x]) cout << "B\n"; // first player loses => B wins
-        else cout << "A\n";          // first player wins => A wins
+        if(islose[x]) cout << "B\n"; // kezdő játékos veszít => B nyer
+        else cout << "A\n";          // kezdő játékos nyer => A nyer
     }
 }
